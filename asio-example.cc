bb966f2f2642b05652cc4aa9a313713f38f7a314
Stuart Taylor <stu@improbable.io>
2016-02-10 17:20:31 +0000

    Error handling.

--- asio-example.cc
+++ asio-example.cc
@@ -6,15 +6,20 @@
 
 using namespace asio::ip;
 
-void Connect(const std::string& host, std::uint16_t port) {
+bool Connect(const std::string& host, std::uint16_t port) {
   asio::io_service service;
+  asio::error_code error_code;
 
   // Resolve host.
   tcp::resolver::query query{host, std::to_string(port)};
   tcp::resolver resolver{service};
-  auto address = resolver.resolve(query);
+  auto address = resolver.resolve(query, error_code);
+  if (error_code) {
+    return false;
+  }
 
   // Connect.
   tcp::socket socket{service};
-  asio::connect(socket, address);
+  asio::connect(socket, address, error_code);
+  return !error_code;
 }
