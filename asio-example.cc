99399ef00591cf6c40c5c496b491ff3424d737f5
Stuart Taylor <stu@improbable.io>
2016-02-10 17:22:22 +0000

    Add a timeout.

--- asio-example.cc
+++ asio-example.cc
@@ -1,12 +1,14 @@
 #include <asio/connect.hpp>
 #include <asio/ip/tcp.hpp>
+#include <asio/steady_timer.hpp>
 #include <asio.hpp>
+#include <chrono>
 #include <cstdint>
 #include <string>
 
 using namespace asio::ip;
 
-bool Connect(const std::string& host, std::uint16_t port) {
+bool Connect(const std::string& host, std::uint16_t port, std::uint64_t timeout_millis) {
   asio::io_service service;
   asio::error_code error_code;
 
@@ -18,10 +20,19 @@ bool Connect(const std::string& host, std::uint16_t port) {
     return false;
   }
 
+  // Start timeout.
+  asio::steady_timer timer{service};
+  timer.expires_from_now(std::chrono::milliseconds(timeout_millis));
+  timer.async_wait([&](asio::error_code) {
+    error_code = asio::error::timed_out;
+    service.stop();
+  });
+
   // Connect.
   tcp::socket socket{service};
   asio::async_connect(socket, address, [&](asio::error_code ec, tcp::resolver::iterator) {
     error_code = ec;
+    service.stop();
   });
 
   asio::error_code service_error_code;
