94740eee5fcdd7d8240545226fa9b0d0a1e6a8ae
Stuart Taylor <stu@improbable.io>
2016-02-10 17:22:42 +0000

    Asynchronous resolve and connect.

--- asio-example.cc
+++ asio-example.cc
@@ -12,14 +12,6 @@ bool Connect(const std::string& host, std::uint16_t port, std::uint64_t timeout_
   asio::io_service service;
   asio::error_code error_code;
 
-  // Resolve host.
-  tcp::resolver::query query{host, std::to_string(port)};
-  tcp::resolver resolver{service};
-  auto address = resolver.resolve(query, error_code);
-  if (error_code) {
-    return false;
-  }
-
   // Start timeout.
   asio::steady_timer timer{service};
   timer.expires_from_now(std::chrono::milliseconds(timeout_millis));
@@ -28,11 +20,22 @@ bool Connect(const std::string& host, std::uint16_t port, std::uint64_t timeout_
     service.stop();
   });
 
-  // Connect.
+  tcp::resolver::query query{host, std::to_string(port)};
+  tcp::resolver resolver{service};
   tcp::socket socket{service};
-  asio::async_connect(socket, address, [&](asio::error_code ec, tcp::resolver::iterator) {
-    error_code = ec;
-    service.stop();
+
+  // Resolve host...
+  resolver.async_resolve(query, [&](asio::error_code ec, const tcp::resolver::iterator& address) {
+    if (ec) {
+      error_code = ec;
+      service.stop();
+    } else {
+      // ... and then connect.
+      asio::async_connect(socket, address, [&](asio::error_code ec, tcp::resolver::iterator) {
+        error_code = ec;
+        service.stop();
+      });
+    }
   });
 
   asio::error_code service_error_code;
